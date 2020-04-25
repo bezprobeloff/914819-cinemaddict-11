a4df5767bf825a6c87c47542a36034449ae24bc8
bezprobeloff <44836223+bezprobeloff@users.noreply.github.com>
2020-04-25 10:13:13 +0300

    Добавлен контроллер movie

---
+++ src/controllers/movie.js
@@ -0,0 +1,13 @@
+import CardFilmComponent from "../components/card-film";
+import FilmPopupComponent from "../components/film-details";
+import {render, append, remove, RenderPosition} from "../utils/render";
+
+export default class MovieController {
+  constructor(container) {
+
+  }
+
+  render(film) {
+
+  }
+}
